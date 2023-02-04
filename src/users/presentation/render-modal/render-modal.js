e1d850c522707cce868d512da4975699e429f069
Juan Diego G <juan.gil.trujillo@correounivalle.edu.co>
2023-02-03 22:42:00 -0500

    evento de formulario parcialmente implementado

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -1,7 +1,7 @@
 import html from './render-modal.html?raw';
 import './render-modal.css';
 
-let modal;
+let modal, form;
 
 export const closeModal = () => {
   modal?.classList.remove('hide-modal');
@@ -9,6 +9,7 @@ export const closeModal = () => {
 
 export const hideModal = () => {
   modal?.classList.add('hide-modal');
+  form?.reset();
 }
 
 /**
@@ -25,9 +26,31 @@ export const renderModal = ( element ) => {
 
   element.append( modal );
 
+  form = modal.querySelector('form');
+
   modal.addEventListener('click', ( event ) => {
     // console.log( !event.target.classList.contains('modal-container') );
     if( !event.target.classList.contains('modal-container')) return;  
+    hideModal();
+  });
+
+  form.addEventListener( 'submit', ( event ) => {
+    event.preventDefault();
+
+    const refForm = new FormData( form );
+
+    const userLike = {};
+    for (const [ key, value ] of refForm) {
+      if ( key === 'balance' ) {
+        userLike[key] = + value;
+        continue;
+      }
+      if ( key === 'isActive' ) {
+        userLike[key] = ( value === 'on' ) ? true : false;
+        continue;
+      }
+      userLike[key] = value; 
+    }
     hideModal();
   })
 }
